e16154c75a4e3dd5b6bc0ff32d61b59d46930735
Bri Dickman <brianna.dickman@gmail.com>
2017-02-19 19:23:53 -0600

    Solved object constructor code in objects.assignment.js

--- assignment/arrays.assignment.js
+++ assignment/arrays.assignment.js
@@ -12,9 +12,12 @@ function createArray() {
 
 function loopArray(array) {
   // used below
-  var conString = '';
+  var conString = ['animal', 'crackers', '13', 'soup'];
   // Write code here: Concatentate all of the values in array together using a loop
+  for(var i = 0; i < conString.length; i++){
 
+    return conString(i) + ;
+  }
 
   // return concatentated values
   return conString;
@@ -25,7 +28,7 @@ function arrayReverser(arrayParam) {
   // Copy items from arrayParam to a new array called reversedArray, reversing the order of elements
   // Last element on arrayParam should become the first in reversedArray
   // Do not use Array.reverse()!
-
+  var reversedArray = [];
   // Write the code here
 
 
--- assignment/objects.assignment.js
+++ assignment/objects.assignment.js
@@ -24,27 +24,29 @@ function threeBooks() {
 
   // Add a property of haveRead to each book object you created. This should be set to true or false
   // Write code here
+book1.haveRead = true;
+book2.haveRead = false;
+book3.haveRead = true;
 
   // Store these objects in an array called books
   // Write code here
-
+var books = [book1, book2, book3];
   // return this array
-  return books;
+return books;
+
 }
 
 function fixLiteralSyntax() {
   // Fix the problems with our object literal
-  /**
-  avengersMovie = {
-      name : Avengers;
-      runtime : 143 mins;
-      releaseYear = 2012;
-      "Director" : "Joss Whedon".
-  }
-  **/
+avengersMovie = {
+      name: "Avengers",
+      runtime: "143 mins",
+      releaseYear: 2012,
+      director: "Joss Whedon"
+  };
 
   // return corrected object
-  return avengersMovie;
+return avengersMovie;
 }
 
 
@@ -54,22 +56,34 @@ Object Constructors
 
 // Write a constructor function to create Book objects with the same four properties as above.
 // Write code here
+function Book(title, author, pageCount, haveRead){
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.haveRead= haveRead;
+}
 
 function createBooksWithConstructor() {
   // Create 3 new books using this constructor function.
   // Write code here
+  var bookA = new Book("Pride and Prejudice", "Jane Austen", 279, false);
+  var bookB = new Book("Casual Vacancy", "J.K. Rowling", 503, false);
+  var bookC = new Book("Outlander", "Diana Gabaldon", 896, true);
 
   // Store them in an array called books
   // Write code here
-
+  var books = [bookA, bookB, bookC];
   // return our array of books
-  return books;
+return books;
 }
+
+createBooksWithConstructor();
+
 
 // -- DON'T TOUCH CODE BELOW --
 module.exports = {
-  threeBooks: threeBooks,
-  fixLiteralSyntax: fixLiteralSyntax,
-  Book: Book,
-  createBooksWithConstructor: createBooksWithConstructor
+threeBooks: threeBooks,
+fixLiteralSyntax: fixLiteralSyntax,
+Book: Book,
+createBooksWithConstructor: createBooksWithConstructor
 };
